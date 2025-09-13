06d997fbb2d6fe94db35a8c6e48c02da469cada7
Aryankr5252 <aryanak5252@gmail.com>
2025-09-13 16:57:25 +0530

    models, database and server.js is done

---
+++ models/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Admin", "Member"],
+      default: "Member",
+    },
+    tenantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tenant",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.generateAuthToken = function() {
+  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {expiresIn: '24h'});
+};
+
+userSchema.methods.comparePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = async (password) => {
+    return await bcrypt.hash(password, 10);
+}
+
+export default mongoose.model("User", userSchema);
