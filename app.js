b011f6fb58c6f5b515833d622fd540936970a868
Aryankr5252 <aryanak5252@gmail.com>
2025-09-15 21:23:11 +0530

    cors changes

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",   
+  origin: [
+    "http://localhost:5173", 
+    "https://your-frontend.vercel.app"
+  ],  
   credentials: true                  
 }));
 
